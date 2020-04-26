504c9f1f2db35da99e23d0ac48fd635709640c36
dinle <dinle@DESKTOP-5TET86P>
2020-04-26 23:17:42 +0900

    g

--- 과제2/소스.cpp
+++ 과제2/소스.cpp
@@ -1,1 +1,22 @@
-2
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+void sum(const char* filename)
+{
+	ifstream table(filename);
+	char line[255];
+	while (!table.eof())
+	{
+		table.getline(line, 255);
+		cout << stoi(line);
+	}
+
+}
+
+int main()
+{
+	sum("Text.txt");
+}
